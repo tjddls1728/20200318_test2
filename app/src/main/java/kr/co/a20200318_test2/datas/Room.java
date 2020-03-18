4c67ed2f080b8aa80fd852e341800b6962c6c7bc
tjddls1728 <61854982+tjddls1728@users.noreply.github.com>
2020-03-18 14:51:18 +0900

    방 데이터 클래스 생성

---
+++ app/src/main/java/kr/co/a20200318_test2/datas/Room.java
@@ -0,0 +1,53 @@
+package kr.co.a20200318_test2.datas;
+
+import java.io.Serializable;
+
+public class Room implements Serializable {
+
+    private int price;
+    private String address;
+    private int floor;
+    private String description;
+
+//    알트 인설트
+
+
+    public Room(int price, String address, int floor, String description) {
+        this.price = price;
+        this.address = address;
+        this.floor = floor;
+        this.description = description;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getFloor() {
+        return floor;
+    }
+
+    public void setFloor(int floor) {
+        this.floor = floor;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
